bbc339251d42238290e1da17cbe7e058c7d4514e
KareemShaban1 <karim17196@fci.bu.edu.eg>
2024-11-12 11:20:02 +0200

    edit order trackings attributes

--- app/Models/OrderTracking.php
+++ app/Models/OrderTracking.php
@@ -16,6 +16,56 @@ class OrderTracking extends Model
      */
     protected $fillable = ['order_id','pending_at','processing_at','shipped_at','canceled_at','completed_at'];
 
+    /**
+     * Convert pending_at to a timestamp if it is set.
+     *
+     * @return int|null
+     */
+    public function getPendingAtAttribute($value)
+    {
+        return $value ? strtotime($value) : null;
+    }
+
+    /**
+     * Convert processing_at to a timestamp if it is set.
+     *
+     * @return int|null
+     */
+    public function getProcessingAtAttribute($value)
+    {
+        return $value ? strtotime($value) : null;
+    }
+
+    /**
+     * Convert shipped_at to a timestamp if it is set.
+     *
+     * @return int|null
+     */
+    public function getShippedAtAttribute($value)
+    {
+        return $value ? strtotime($value) : null;
+    }
+
+    /**
+     * Convert canceled_at to a timestamp if it is set.
+     *
+     * @return int|null
+     */
+    public function getCanceledAtAttribute($value)
+    {
+        return $value ? strtotime($value) : null;
+    }
+
+    /**
+     * Convert completed_at to a timestamp if it is set.
+     *
+     * @return int|null
+     */
+    public function getCompletedAtAttribute($value)
+    {
+        return $value ? strtotime($value) : null;
+    }
+
     public function order(){
         return $this->belongsTo(Order::class);
     }
