42ee89895f380207cceb8429482f452f23d0efda
KareemShaban1 <karim17196@fci.bu.edu.eg>
2024-11-12 11:37:00 +0200

    edit order trackings attributes

--- app/Models/OrderTracking.php
+++ app/Models/OrderTracking.php
@@ -4,66 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class OrderTracking extends Model
 {
     use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = ['order_id', 'pending_at', 'processing_at', 'shipped_at', 'canceled_at', 'completed_at'];
 
-    /**
-     * Convert pending_at to 'Y-m-d H:i:s' format if it is set.
-     *
-     * @return string|null
-     */
     public function getPendingAtAttribute($value)
     {
-        return $value ? \Carbon\Carbon::parse($value)->format('Y-m-d H:i:s') : null;
+        return $value ? Carbon::parse($value)->toISOString() : null;
     }
 
-    /**
-     * Convert processing_at to 'Y-m-d H:i:s' format if it is set.
-     *
-     * @return string|null
-     */
     public function getProcessingAtAttribute($value)
     {
-        return $value ? \Carbon\Carbon::parse($value)->format('Y-m-d H:i:s') : null;
+        return $value ? Carbon::parse($value)->toISOString() : null;
     }
 
-    /**
-     * Convert shipped_at to 'Y-m-d H:i:s' format if it is set.
-     *
-     * @return string|null
-     */
     public function getShippedAtAttribute($value)
     {
-        return $value ? \Carbon\Carbon::parse($value)->format('Y-m-d H:i:s') : null;
+        return $value ? Carbon::parse($value)->toISOString() : null;
     }
 
-    /**
-     * Convert canceled_at to 'Y-m-d H:i:s' format if it is set.
-     *
-     * @return string|null
-     */
     public function getCanceledAtAttribute($value)
     {
-        return $value ? \Carbon\Carbon::parse($value)->format('Y-m-d H:i:s') : null;
+        return $value ? Carbon::parse($value)->toISOString() : null;
     }
 
-    /**
-     * Convert completed_at to 'Y-m-d H:i:s' format if it is set.
-     *
-     * @return string|null
-     */
     public function getCompletedAtAttribute($value)
     {
-        return $value ? \Carbon\Carbon::parse($value)->format('Y-m-d H:i:s') : null;
+        return $value ? Carbon::parse($value)->toISOString() : null;
     }
 
     public function order()
