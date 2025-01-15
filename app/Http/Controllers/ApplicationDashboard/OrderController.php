1aac2e88a7f5b7ef6263a12cb3279de749a23a0a
KareemShaban1 <karim17196@fci.bu.edu.eg>
2025-01-15 16:44:01 +0200

    fix issues

--- app/Http/Controllers/ApplicationDashboard/OrderController.php
+++ app/Http/Controllers/ApplicationDashboard/OrderController.php
@@ -430,8 +430,14 @@ class OrderController extends Controller
         // Fetch activity logs related to the order
         $activityLogs = Activity::with(['subject'])
             ->leftJoin('users as u', 'u.id', '=', 'activity_log.causer_id')
-            ->leftJoin('clients as c', 'c.id', '=', 'activity_log.causer_id')
-            ->leftJoin('deliveries as d', 'd.id', '=', 'activity_log.causer_id')
+            ->leftJoin('clients as c', function($join) {
+                $join->on('c.id', '=', 'activity_log.causer_id')
+                     ->where('activity_log.causer_type', '=', 'App\Models\Client');
+            })
+            ->leftJoin('deliveries as d', function($join) {
+                $join->on('d.id', '=', 'activity_log.causer_id')
+                     ->where('activity_log.causer_type', '=', 'App\Models\Delivery');
+            })
             ->leftJoin('contacts as contact', function ($join) {
                 $join->on('contact.id', '=', 'c.contact_id')
                     ->orOn('contact.id', '=', 'd.contact_id');
