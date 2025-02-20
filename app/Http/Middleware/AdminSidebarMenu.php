ccbd3a794254d458eb0e12d149fa7189a93db0bd
KareemShaban1 <karim17196@fci.bu.edu.eg>
2025-02-20 21:42:51 +0200

    new updates

--- app/Http/Middleware/AdminSidebarMenu.php
+++ app/Http/Middleware/AdminSidebarMenu.php
@@ -109,7 +109,7 @@ class AdminSidebarMenu
                             );
                         }
 
-                        dd(dd(config('app.GOOGLE_MAP_API_KEY')));
+                        dd(config('app.GOOGLE_MAP_API_KEY'));
                         if(!empty(env('GOOGLE_MAP_API_KEY'))) {
                             $sub->url(
                                 action('ContactController@contactMap'),
