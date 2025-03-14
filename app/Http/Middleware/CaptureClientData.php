d8555b7d31b18f343dc7c75ca949d746ba56ebbe
KareemShaban1 <karim17196@fci.bu.edu.eg>
2025-03-14 14:50:36 +0200

    new updates

--- app/Http/Middleware/CaptureClientData.php
+++ app/Http/Middleware/CaptureClientData.php
@@ -3,63 +3,48 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class CaptureClientData
 {
     /**
      * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next)
     {
-        // Get Google API Key from .env
-        // $googleApiKey = env('GOOGLE_MAP_API_KEY');
-        $googleApiKey = 'AIzaSyBUK88jmlcZv3IdJlhp944cJmzkWKelqq4';
-
         // Get User IP
         $ip = $request->ip();
 
         // Get User Agent
         $userAgent = $request->header('User-Agent');
 
-        $locationResponse = Http::withHeaders([
-            'Content-Type' => 'application/json'
-        ])->post("https://www.googleapis.com/geolocation/v1/geolocate?key={$googleApiKey}", [
-            'considerIp' => false, // This tells the API to use the IP address for geolocation
-        ])->json();
-        
-        \Log::info('locationResponse', [$locationResponse]);
-        
-        
-        
-        if (isset($locationResponse['location'])) {
-            $lat = $locationResponse['location']['lat'];
-            $lng = $locationResponse['location']['lng'];
-        
-            // Convert Lat/Lon to Address using Google Geocoding API
-            $addressResponse = Http::get("https://maps.googleapis.com/maps/api/geocode/json?latlng={$lat},{$lng}&key={$googleApiKey}")->json();
-        
-            $formattedAddress = $addressResponse['results'][0]['formatted_address'] ?? 'Address not found';
-        
-            // Attach user data to the request
-            $request->merge([
-                'user_data' => [
-                    'ip' => $ip,
-                    'user_agent' => $userAgent,
-                    'latitude' => $lat,
-                    'longitude' => $lng,
-                    'address' => $formattedAddress
-                ]
-            ]);
-        
-            // Log client data
-            \Log::info('Client Data:', $request->user_data);
-        } else {
-            \Log::error('Failed to retrieve location');
-            return response()->json(['error' => 'Failed to retrieve location'], 500);
-        }
-        
+        // Get Location (Using ip-api.com)
+        $location = Http::get("http://ip-api.com/json/{$ip}")->json();
+
+        // Attach user data to the request object
+        $request->merge([
+            'user_data' => [
+                'ip' => $ip,
+                'user_agent' => $userAgent,
+                'location' => $location
+            ]
+        ]);
+
+        $clientData = [
+            'client_data' => [
+                'ip' => $ip,
+                'user_agent' => $userAgent,
+                'location' => $location
+            ]
+        ];
+
+        \Log::info('client_data',[$clientData]);
+
         return $next($request);
+    
     }
 }
