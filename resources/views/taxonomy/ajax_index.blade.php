dc1804f57339f4624da8e1dda228fe7f37985619
KareemShaban1 <karim17196@fci.bu.edu.eg>
2025-08-22 16:13:33 +0300

    new updates 22/8/2025

--- resources/views/taxonomy/ajax_index.blade.php
+++ resources/views/taxonomy/ajax_index.blade.php
@@ -1,20 +1,14 @@
-@php
-    $is_cat_code_enabled = isset($module_category_data['enable_taxonomy_code']) && !$module_category_data['enable_taxonomy_code'] ? false : true;
-@endphp
-@can('category.create')
-	<button type="button" class="btn btn-sm pull-right btn-primary btn-modal" data-href="{{action('TaxonomyController@create')}}?type={{$category_type}}" data-container=".category_modal">
-		<i class="fa fa-plus"></i>
-		@lang( 'messages.add' )
-	</button>
-	<br><br>
-@endcan
-
- @can('category.view')
+@can('category.view')
     <div class="table-responsive">
         <table class="table table-bordered table-striped" id="category_table" style="width: 100%;">
             <thead>
                 <tr>
-                    <th>@if(!empty($module_category_data['taxonomy_label'])) {{$module_category_data['taxonomy_label']}} @else @lang( 'category.category' ) @endif</th>
+                    <th>@if(!empty($module_category_data['taxonomy_label']))
+                        {{$module_category_data['taxonomy_label']}}
+                        @else
+                        @lang( 'category.category' )
+                        @endif
+                    </th>
                     @if($is_cat_code_enabled)
                         <th>{{ $module_category_data['taxonomy_code_label'] ?? __( 'category.code' )}}</th>
                     @endif
@@ -25,6 +19,3 @@
         </table>
     </div>
 @endcan
-
-<div class="modal fade category_modal" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
-</div>
